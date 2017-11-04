409f1f748e7e882c9cd570b4cbf2e72c4863f52d
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2017-11-04 21:42:00 +0000

    Quran v1.11.4 responsive

--- script/elementsHTML.js
+++ script/elementsHTML.js
@@ -1,17 +1,60 @@
 /* created bY MOHSSINE_ABOUTAJ 1438/2017 */
 /* Scrtpts for LOGO , HEADER , FOOTER */
+"use strict";
 
-(function () {
-    "use strict";
+// Button scroll to up
+var btn = document.createElement('div');
 
-    // footer Elements 
-    var footer = document.getElementById('footer'),
-        right = document.createElement('div'),
-        left = document.createElement('div');
+btn.classList = 'fa fa-arrow-up toUp';
 
-    right.innerHTML = "جميع الحقوق &copy; محفوظة لاصحابها";
-    left.innerHTML = 'تصميم و برمجة <b>محسن ابوتاج</b> 1438/2017';
-    
-    footer.appendChild(right);
-    footer.appendChild(left);
-})();
+btn.onclick = function(){
+    window.scrollTo(0,0);
+};
+
+setInterval(function(){
+    if(window.pageYOffset <= window.innerHeight){
+        btn.style.display = 'none';
+    } else {
+        btn.style.display = 'block';
+    }
+},1000)
+
+document.body.appendChild(btn);
+
+// navigation Bar
+var nav = document.querySelector('nav');
+
+window.onscroll = function(){
+    if(window.pageYOffset <= '100'){
+        nav.style.top = '0px';
+    } else {
+        nav.style.top = '-68px';
+    }
+};
+
+nav.onmouseover = function(){
+    nav.style.top = '0px';
+};
+
+// dropdown Botton
+var drop = document.querySelector('.drop'),
+    links = document.querySelector('nav').querySelector('ul');
+
+drop.onclick = function(){
+    if(links.style.display == 'block'){
+        links.style.display = 'none';
+    } else {
+        links.style.display = 'block';
+    }
+};
+
+// footer Elements 
+var footer = document.getElementById('footer'),
+    right = document.createElement('div'),
+    left = document.createElement('div');
+
+right.innerHTML = "جميع الحقوق &copy; محفوظة لاصحابها";
+left.innerHTML = "تصميم و برمجة <a class='no-decore' href='#'><b>محسن ابوتاج</b></a> 1438/2017";
+
+footer.appendChild(right);
+footer.appendChild(left);
--- script/quranScripts.js
+++ script/quranScripts.js
@@ -1,7 +1,7 @@
 /* created bY MOHSSINE_ABOUTAJ 1438/2017 */
 /* Scripts for Quran playlist and Quran Tabke */
-
-
+// Use strict mode 
+"use strict";
 var control = document.getElementById('control'),
     playNow = document.getElementById('playNow'),
     quranList = ["alfatiha 1", "alfatiha 2", "alfatiha 3", "114"],
@@ -32,14 +32,17 @@ var control = document.getElementById('control'),
     }
 })();
 
+// about sorah is playing
 function sorahInfo(index){
-    document.querySelector('.sorahInfo').innerHTML = "<table class='no-border-all'>" +
+    document.querySelector('.sorahInfo').innerHTML = 
+        "<table class='no-border-all'>" +
         "<tr><td>القارئ الشيخ :</td><td>" + alqari2[index] + "</td>" +
         "<tr><td>سورة :</td><td>" + soraName[index] + "</td>" +
         "<tr><td>المدة :</td><td>" + Math.floor(playNow.duration / 60) + ":" + Math.round(playNow.duration % 60) + "</td>"
         + "</table>";
 }
 
+// play and pause sorah playing
 control.onclick = function () {
     control.classList.toggle('fa-play');
     control.classList.toggle('fa-pause');
@@ -57,8 +60,13 @@ document.querySelector('.fa-random').onclick = function(){
     playNow.src = quranFolder + quranList[x] + ext;
     playNow.play();
     sorahInfo(x);
+    forward(x);
+    backward(x);
+    control.classList.remove('fa-play');
+    control.classList.add('fa-pause');
 };
 
+// replay the sorah is playing
 document.querySelector('.fa-retweet').onclick = function(){
     if(playNow.hasAttribute('loop')){
         this.style.color = '#b19393';
@@ -69,19 +77,25 @@ document.querySelector('.fa-retweet').onclick = function(){
     }
 };
 
-document.querySelector('.fa-fast-forward').onclick = function(){
-    if(current + 1 == quranList.length ){ current = -1 }
-    playNow.src = quranFolder + quranList[current += 1] + ext;
-    playNow.play();
-    sorahInfo(current);
-};
+// Forward function
+function forward(current){
+    document.querySelector('.fa-fast-forward').onclick = function(){
+        if(current + 1 == quranList.length ){ current = -1 }
+        playNow.src = quranFolder + quranList[current += 1] + ext;
+        playNow.play();
+        sorahInfo(current);
+    };
+}
 
-document.querySelector('.fa-fast-backward').onclick = function(){
-    if(current - 1 < 0 ){ current = quranList.length }
-    playNow.src = quranFolder + quranList[current -= 1] + ext;
-    playNow.play();
-    sorahInfo(current);
-};
+// Backward function
+function backward(current){
+    document.querySelector('.fa-fast-backward').onclick = function(){
+        if(current - 1 < 0 ){ current = quranList.length }
+        playNow.src = quranFolder + quranList[current -= 1] + ext;
+        playNow.play();
+        sorahInfo(current);
+    };
+}
 
 
 window.onload = setTimeout(function playAuto() {
@@ -89,7 +103,9 @@ window.onload = setTimeout(function playAuto() {
     playNow.play();
     control.classList.remove('fa-play');
     control.classList.add('fa-pause');
-    sorahInfo(current);
+    setTimeout(sorahInfo(current),3333);
+    forward(current);
+    backward(current);
 
     // Set the 'ended' event here, so it will run playNext() function each time a surah ended
     // and it will play the next surah.
@@ -108,4 +124,6 @@ function playNext(){
     playNow.src = quranFolder + quranList[current] + ext;
     playNow.play();
     sorahInfo(current);
+    forward(current);
+    backward(current);
 }
